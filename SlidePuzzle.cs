c2371c8fc39919b5b85e1f2d96315e4490b346f8
okayu <34333583+okayun@users.noreply.github.com>
2018-05-30 03:20:21 +0900

    Update SlidePuzzle.cs

--- SlidePuzzle.cs
+++ SlidePuzzle.cs
@@ -8,7 +8,6 @@ using System.Windows.Forms;
 
 class Program
 {
-
     [STAThread]
     public static void Main()
     {
@@ -20,7 +19,8 @@ class SlidePuzzle
 {
     Form form;
     PictureBox[,] pics;
-    int height, width;
+    int height, width; // 画像 1 枚あたりの縦, 横の長さ
+    int H, W; // H × W 枚の画像
     int[] vx = { -1, 0, 1, 0 };
     int[] vy = { 0, -1, 0, 1 };
 
@@ -31,23 +31,27 @@ class SlidePuzzle
         form.Text = "Puzzle";
         form.KeyPreview = true;
 
-        pics = new PictureBox[3, 3];
+        H = W = 3;
 
-        for (int i = 0; i < 3; ++i)
+        pics = new PictureBox[H, W];
+
+        for (int i = 0; i < H; ++i)
         {
             int lx = 0, ly = i * 111;
 
-            for (int j = 0; j < 3; ++j)
+            for (int j = 0; j < W; ++j)
             {
                 pics[i, j] = new PictureBox();
 
                 if (i == 2 && j == 2)
                 {
+                    // ここ相対パスにしたいけどなんかエラー出る
                     pics[i, j].Image = Image.FromFile(@"C:\Users\yamaoka\source\repos\Puzzle\img\blank.png");
                     pics[i, j].Text = "blank";
                 }
                 else
                 {
+                    // ここも
                     pics[i, j].Image = Image.FromFile(@"C:\Users\yamaoka\source\repos\Puzzle\img\1-" + (i + 1) + "-" + (j + 1) + ".png");
                 }
 
@@ -73,15 +77,15 @@ class SlidePuzzle
 
         Point cp = form.PointToClient(Cursor.Position);
 
-        for (int i = 0; i < 3; ++i)
+        for (int i = 0; i < H; ++i)
         {
-            for (int j = 0; j < 3; ++j)
+            for (int j = 0; j < W; ++j)
             {
                 if (pics[i, j].Left <= cp.X && cp.X < pics[i, j].Left + width && pics[i, j].Top <= cp.Y && cp.Y < pics[i, j].Top + height)
                 {
-                    for (int m = 0; m < 3; ++m)
+                    for (int m = 0; m < H; ++m)
                     {
-                        for (int n = 0; n < 3; ++n)
+                        for (int n = 0; n < W; ++n)
                         {
                             if (i == m && j == n)
                             {
@@ -93,7 +97,7 @@ class SlidePuzzle
                                 int x = pics[i, j].Left + vx[k] * width;
                                 int y = pics[i, j].Top + vy[k] * height;
 
-                                if (0 <= x && x < height * 3 && 0 <= y && y < width * 3)
+                                if (0 <= x && x < height * H && 0 <= y && y < width * W)
                                 {
                                     if (pics[m, n].Left == x && pics[m, n].Top == y && pics[m, n].Text == "blank")
                                     {
@@ -109,10 +113,10 @@ class SlidePuzzle
                     }
                 }
             }
-
         }
     }
 
+    // 結局使わなかった
     void Swap<T>(ref T a, ref T b)
     {
         T tmp = a;
